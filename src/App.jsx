85be8be662df79acb5ae8f4be9acc9c73914dd52
vyshakh <vyshakh0015@gmail.com>
2025-08-01 15:59:59 +0400

    Initial commit – Supreme Facilities website setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Supreme Facilities</h1>
+      <p>Welcome to the Facility Management site!</p>
+    </div>
+  )
+}
+
+export default App
